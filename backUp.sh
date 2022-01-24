7f94b3b3c654c027896dcb5d6ee41492646c5af5
anilprajapati <anilprajapati18@gnu.ac.in>
2022-01-24 14:55:51 +0530

    6th done

--- backUp.sh
+++ backUp.sh
@@ -1,5 +1,8 @@
 backUp(){
-    mkdir /etc/backup
+    if  [[ "backup" != "$(ls /etc/ | grep -o backup)" ]]
+    then
+        mkdir /etc/backup
+    fi    
 	zip /etc/backup/mysqlBackup"$(date +%d-%m-%y-%T)".zip /var/lib/mysql/*
 
 }
